6d2301fe26aa0c4893a838cab68713e348ca82af
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-06-27 11:49:14 -0500

    Time: 1 ms (99.56%), Space: 44.1 MB (71.30%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> finalList = new ArrayList<>(n); // creates a new ArrayList with length n
+        
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                finalList.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                finalList.add("Fizz");
+            } else if(i % 5 == 0) {
+                finalList.add("Buzz");
+            } else {
+                finalList.add(String.valueOf(i));
+            }
+        }
+        
+        return finalList;
+    }
+}
