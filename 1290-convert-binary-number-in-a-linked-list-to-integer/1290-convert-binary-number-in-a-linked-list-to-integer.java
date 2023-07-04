49f01a42d5c9c14be8858aa0f5f74adde8cb67c3
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-04 16:29:01 -0500

    Time: 0 ms (100.00%), Space: 40 MB (80.82%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -15,6 +15,7 @@ class Solution {
         int n = 0;
         int exponent = 0;
     
+//         this is how you reverse a LinkedList (above and below)
         while(current != null) { 
             ListNode next = current.next; 
             current.next = prev;
@@ -22,12 +23,14 @@ class Solution {
             current = next;
         }
         
+//         now that the linkedlist is reversed, we can calculate the decimal value using the formula (googled formula)
         while(prev.next != null) {
             n += prev.val * Math.pow(2, exponent);
             prev = prev.next;
             exponent++;
         }
         
+//         this is to get the last value in the linked list, because prev.next == null ends it before it does the logic inside of the while loop right before the null node
         n += prev.val * Math.pow(2, exponent);
         
         return n;
