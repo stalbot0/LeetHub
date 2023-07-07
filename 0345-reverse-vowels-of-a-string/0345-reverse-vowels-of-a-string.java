b828a9cf0cfd3c56923729d5a0caf32473d345d8
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-07 12:10:07 -0500

    Time: 3 ms (97.60%), Space: 43.5 MB (89.05%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
-        char arr[] = s.toCharArray();
-        int i = 0, j = s.length() - 1;
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
         
         while (i < j) {
-            if (arr[i] != 'a' && arr[i] != 'e' && arr[i] != 'i' && arr[i] != 'o' && arr[i] != 'u' &&
-                arr[i] != 'A' && arr[i] != 'E' && arr[i] != 'I' && arr[i] != 'O' && arr[i] != 'U'
-               ) {
+            if (isNotVowel(arr[i])) {
                 i++;
-            } else if (arr[j] != 'a' && arr[j] != 'e' && arr[j] != 'i' && arr[j] != 'o' && arr[j] !=                        'u' && arr[j] != 'A' && arr[j] != 'E' && arr[j] != 'I' && arr[j] != 'O' &&                            arr[j] != 'U') {
+            } else if (isNotVowel(arr[j])) {
                 j--;
             } else {
-                char ch = arr[i];
+                char temp = arr[i];
                 arr[i] = arr[j];
-                arr[j] = ch;
+                arr[j] = temp;
                 i++;
                 j--;
             }
         }
         
         return new String(arr);
+    }
+    
+    public boolean isNotVowel(char c) {
+        return c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && c != 'A' && c != 'E' && c                != 'I' && c != 'O' && c != 'U';
     }
 }
