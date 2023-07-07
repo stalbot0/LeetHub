7e58016c4c5b4f401d0ecc1e7cd5af8ba0df3d9b
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-07 10:59:17 -0500

    Time: 1 ms (99.27%), Space: 44 MB (78.72%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
+        int plantedFlowers = 0;
+        boolean plantedAllFlowers = false;
         for (int i = 0; i < flowerbed.length; i++) {
-            // Check if the current plot is empty.
+//          Check if the current plot is empty.
             if (flowerbed[i] == 0) {
-                // Check if the left and right plots are empty.
+//                  check to see if we are at 0 or if the plot to the left is 0
                 boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+//                  checks to see if we are at the end of the flowerbed or if the plot to the right is 0
                 boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
                 
-                // If both plots are empty, we can plant a flower here.
+//                  if both plots are empty (0), then we can plant
                 if (emptyLeftPlot && emptyRightPlot) {
+//                     set the flowerbed at our current index to 1 ie we just planted
                     flowerbed[i] = 1;
-                    count++;
+//                     increment the number of planted flowers
+                    plantedFlowers++;
                 }
             }
         }
-        return count >= n;
+//         if we planted n or more flowers, then we were able to plant all n flowers with no isses
+        if (plantedFlowers >= n) {
+            plantedAllFlowers = true;
+        }
+        
+        return plantedAllFlowers;
     }
 }
