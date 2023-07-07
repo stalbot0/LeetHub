3ec82bc24d3658859e1ac6a15757e4999a5bbd11
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-07 09:50:53 -0500

    Time: 10 ms (8.85%), Space: 42 MB (22.15%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,18 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-//         trim both so there are no spaces
+//         replace all spaces in both so there is no white space
         word1.replaceAll(" ", "");
         word2.replaceAll(" ", "");
+//         get the lengths of each for iteration
         int word1Len = word1.length();
         int word2Len = word2.length();
+//         get the max length so when we iterate it will loop through to the longest string
         int maxLength = Math.max(word1Len, word2Len);
-        
+//         initialize our string to concatenate to
         String merged = "";
-        
+//         iterate through 
         for (int i = 0; i < maxLength; i++) {
+//             check if i is less than the first word length and add the character at i
             if (i < word1Len) {
                 merged += word1.charAt(i);
             }
+//             check if i is less than the second word length and add the character at i
             if (i < word2Len) {
                 merged += word2.charAt(i);
             }
