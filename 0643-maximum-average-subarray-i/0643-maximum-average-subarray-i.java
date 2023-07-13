232cce1b138cf3aac16c3091c7559ccd13335bea
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-13 09:48:25 -0500

    Time: 3 ms (94.47%), Space: 54.1 MB (94.53%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,14 +1,11 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         double ans = 0.0;
-        
         int currWindow = 0;
-        
 //         this will give us the sum of the first k values in nums
         for (int i = 0; i < k; i++) {
             currWindow += nums[i];
         }
-        
 //         set the max to the current window to compare
         int maxSum = currWindow;
         
