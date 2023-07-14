d696b1451d5f84e38d1f5cf78da9ff9efc0ca81e
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-14 10:32:07 -0500

    Time: 1 ms (89.14%), Space: 40.7 MB (32.46%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,7 +3,6 @@ class Solution {
         // my first thought was to use a HashMap for key value pairs, and then check the longer string to see if t contains each character in s. Then I wanted to try out the two pointer method, so here we go....
         int left = s.length(), right = t.length(); 
         int p1 = 0, p2 = 0; //two pointers
-        
         // loop through and check the characters at each pointer, if the characters are equal, move the pointers, if the character at t is not equal, 
         while (p1 < left && p2 < right) {
             // check the condition, move p1 if its equal
@@ -13,7 +12,6 @@ class Solution {
             // always increment p2 anyway after the condition is checked
             p2++;
         }
-        
         return p1 == left;
     }
 }
