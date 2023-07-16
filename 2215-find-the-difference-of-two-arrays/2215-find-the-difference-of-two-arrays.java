30dd47e8958888a2da94f0e41d6add6942ac4f1c
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-15 19:40:33 -0500

    Time: 10 ms (96.56%), Space: 44.7 MB (61.56%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> theList = new ArrayList<>();
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
-        
+//         put each of the integers in nums1 & nums2 in HashSets
         Set<Integer> set1 = new HashSet<>();
         for (int n : nums1) {
             set1.add(n);
@@ -13,7 +13,7 @@ class Solution {
         for (int n : nums2) {
             set2.add(n);
         }
-        
+//         iterate through each set and check if the values dont exist in the other set then add them to the proper arraylist
         for (Integer n : set1) {
             if (!set2.contains(n)) {
                 list1.add(n);
@@ -25,7 +25,7 @@ class Solution {
                 list2.add(n);
             }
         }
-        
+//         add both array lists to our 2D/nested list
         theList.add(list1);
         theList.add(list2);
         
