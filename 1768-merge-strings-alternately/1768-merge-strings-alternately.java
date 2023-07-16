dc5eedf28465aea5c85e6ae54973753345e0e987
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-15 19:17:37 -0500

    Time: 10 ms (9.28%), Space: 42.2 MB (6.79%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,28 +1,21 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-//         replace all spaces in both so there is no white space
+//         given to strings, replace all of the spaces to condense the words
         word1.replaceAll(" ", "");
         word2.replaceAll(" ", "");
-//         get the lengths of each for iteration
-        int word1Len = word1.length();
-        int word2Len = word2.length();
-//         get the max length so when we iterate it will loop through to the longest string
-        int maxLength = Math.max(word1Len, word2Len);
-//         initialize our string to concatenate to
-        String merged = "";
-//         iterate through 
-        for (int i = 0; i < maxLength; i++) {
-//             check if i is less than the first word length and add the character at i
-            if (i < word1Len) {
-                merged += word1.charAt(i);
+//         get the max length so that way we can iterate to the very end
+        int max = Math.max(word1.length(), word2.length());
+        String merge = "";
+//         iterate through, check the conditions, and concat to the string
+        for(int i = 0; i < max; i++) {
+            if (i < word1.length()) {
+                merge += word1.charAt(i);
             }
-//             check if i is less than the second word length and add the character at i
-            if (i < word2Len) {
-                merged += word2.charAt(i);
+            
+            if (i < word2.length()) {
+                merge += word2.charAt(i);
             }
         }
-        
-        return merged;
-        
+        return merge;
     }
 }
