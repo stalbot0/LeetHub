ae424e2f77ba9128bb0bb806349de76e945971d5
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-18 11:03:05 -0500

    Time: 67 ms (30.73%), Space: 42.1 MB (80.64%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,20 +4,23 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    var maxCandies = candies[0];
-    for (let i = 1; i <= candies.length; i++) {
-        if (candies[i] > maxCandies) {
-            maxCandies = candies[i];
-        }
-    }
-    var bools = [];
-    candies.forEach(function(candy) {
-       if (candy + extraCandies >= maxCandies) {
-           bools.push(true);
-       } else {
-           bools.push(false);
-       }
-    });
-    
+    const maxCandies = Math.max(...candies);
+    const bools = candies.map(candy => candy + extraCandies >= maxCandies);
     return bools;
+//     var maxCandies = candies[0];
+//     for (let i = 1; i <= candies.length; i++) {
+//         if (candies[i] > maxCandies) {
+//             maxCandies = candies[i];
+//         }
+//     }
+//     var bools = [];
+//     candies.forEach(function(candy) {
+//        if (candy + extraCandies >= maxCandies) {
+//            bools.push(true);
+//        } else {
+//            bools.push(false);
+//        }
+//     });
+    
+//     return bools;
 };
