6a478bf6f82122c0c15801c06543fe633fa88ea0
Stephen Talbot <118476001+stalbot0@users.noreply.github.com>
2023-07-22 17:15:13 -0500

    Time: 0 ms (100.00%), Space: 44.7 MB (24.57%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,11 +1,26 @@
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int[] funnyArr = Arrays.copyOf(nums, (nums.length * 2));
+//         int pointer = 0;
+//         for (int i = (funnyArr.length / 2); i < funnyArr.length; i++) {
+//             funnyArr[i] = nums[pointer];
+//             pointer++;
+//         }
+//         return funnyArr;
+//     }
+// }
+
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] funnyArr = Arrays.copyOf(nums, (nums.length * 2));
-        int pointer = 0;
-        for (int i = (funnyArr.length / 2); i < funnyArr.length; i++) {
-            funnyArr[i] = nums[pointer];
-            pointer++;
-        }
-        return funnyArr;
+        int n = nums.length;
+        int[] concatenatedArr = new int[n * 2];
+        
+        // Copy the original array to the beginning of the concatenated array
+        System.arraycopy(nums, 0, concatenatedArr, 0, n);
+        
+        // Copy the original array again to the end of the concatenated array
+        System.arraycopy(nums, 0, concatenatedArr, n, n);
+        
+        return concatenatedArr;
     }
 }
